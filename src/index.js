5935643fae14e6bfdeb834e7febd8d0181efab09
Ayush Sharma <ayush.sharma@innovaccer.com>
2017-07-06 02:19:01 +0530

    container directive test

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@
  */
 import app from './app.js';
 import './services/wizard.js';
-import container from './directives/container/container.js';
+import './directives/container/container.js';
 import panel from './directives/controls/panel.js';
 import steps from './directives/steps/steps.js';
 import validation from './directives/steps/validation.js';
 
 app
-.directive('formWizard', container)
 .directive('controls', panel)
 .directive('steps', steps)
 .directive('formStepValidity', validation);
